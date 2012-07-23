12f75224addaea051ea7a7079ec21b38b3508918
alexandr.serkov@gmail.com <alexandr.serkov@gmail.com@f8454b35-cfc1-02ba-faa6-ef0c7441a174>
2012-07-23 13:23:12 +0000

    fixed file headers

--- trunk/WirtualJoy/test.m
+++ trunk/WirtualJoy/test.m
@@ -3,7 +3,7 @@
 //  driver
 //
 //  Created by alxn1 on 17.07.12.
-//  Copyright 2012 Dr. Web. All rights reserved.
+//  Copyright 2012 alxn1. All rights reserved.
 //
 
 #import <WirtualJoy/WJoyDevice.h>
