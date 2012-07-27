24b809f2c8e7ff5d14e892d72590736bbdbfaf78
alexandr.serkov@gmail.com <alexandr.serkov@gmail.com@f8454b35-cfc1-02ba-faa6-ef0c7441a174>
2012-07-27 13:14:08 +0000

    Removed LoginItem application, now it's not needed

--- trunk/external utils/LoginItem/LoginItem.m
+++
@@ -1,9 +0,0 @@
-#import "LoginItemsList.h"
-
-int main (int argc, const char *argv[])
-{
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-    NSLog(@"%@", [[LoginItemsList userItemsList] allPaths]);
-    [pool release];
-    return 0;
-}
