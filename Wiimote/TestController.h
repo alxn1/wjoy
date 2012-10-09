0625db7fb1a07f675dc07a2ebe68051c14faf3be
alexandr.serkov <alexandr.serkov@gmail.com>
2012-10-09 06:53:57 +0000

    new test application interface

---
+++ Wiimote/TestController.h
@@ -0,0 +1,26 @@
+//
+//  TestController.h
+//  Wiimote
+//
+//  Created by alxn1 on 09.10.12.
+//  Copyright 2012 Dr. Web. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "WiimoteWrapper.h"
+
+@interface TestController : NSObject
+{
+    @private
+        IBOutlet NSTextView     *m_Log;
+        IBOutlet NSButton       *m_DiscoveryButton;
+        IBOutlet NSTextField    *m_ConnectedTextField;
+
+        NSUInteger               m_ConnectedWiimotes;
+        BOOL                     m_IsDiscovering;
+}
+
+- (IBAction)discovery:(id)sender;
+- (IBAction)clearLog:(id)sender;
+
+@end
---
+++ Wiimote/TestController.m
@@ -0,0 +1,101 @@
+//
+//  TestController.m
+//  Wiimote
+//
+//  Created by alxn1 on 09.10.12.
+//  Copyright 2012 Dr. Web. All rights reserved.
+//
+
+#import "TestController.h"
+
+@implementation TestController
+
+- (void)updateWindowState
+{
+    [m_DiscoveryButton setEnabled:
+                            (!m_IsDiscovering) &&
+                            (m_ConnectedWiimotes == 0)];
+
+    [m_ConnectedTextField setStringValue:
+                            ((m_ConnectedWiimotes == 0)?
+                                (@"No wii remote connected"):
+                                (@"Wii remote connected"))];
+}
+
+- (void)awakeFromNib
+{
+    [WiimoteWrapper setLog:self];
+
+    [[NSNotificationCenter defaultCenter]
+                                    addObserver:self
+                                       selector:@selector(discoveryBegin)
+                                           name:WiimoteBeginDiscoveryNotification
+                                         object:nil];
+
+    [[NSNotificationCenter defaultCenter]
+                                    addObserver:self
+                                       selector:@selector(discoveryEnd)
+                                           name:WiimoteEndDiscoveryNotification
+                                         object:nil];
+
+    [[NSNotificationCenter defaultCenter]
+                                    addObserver:self
+                                       selector:@selector(wiimoteConnected)
+                                           name:WiimoteConnectedNotification
+                                         object:nil];
+
+    [[NSNotificationCenter defaultCenter]
+                                    addObserver:self
+                                       selector:@selector(wiimoteDisconnected)
+                                           name:WiimoteDisconnectedNotification
+                                         object:nil];
+
+    [self updateWindowState];
+}
+
+- (IBAction)discovery:(id)sender
+{
+    [WiimoteWrapper discoveryNew];
+}
+
+- (IBAction)clearLog:(id)sender
+{
+    [m_Log setString:@""];
+}
+
+- (void)wiimoteWrapper:(WiimoteWrapper*)wrapper log:(NSString*)logLine
+{
+    NSAttributedString *tmp = [[NSAttributedString alloc]
+                                    initWithString:[NSString stringWithFormat:@"%@\n", logLine]];
+
+    [[m_Log textStorage] appendAttributedString:tmp];
+    [tmp release];
+}
+
+- (void)discoveryBegin
+{
+    m_IsDiscovering = YES;
+    [self updateWindowState];
+    [self wiimoteWrapper:nil log:@"Begin discovery..."];
+}
+
+- (void)discoveryEnd
+{
+    m_IsDiscovering = NO;
+    [self updateWindowState];
+    [self wiimoteWrapper:nil log:@"End discovery"];
+}
+
+- (void)wiimoteConnected
+{
+    m_ConnectedWiimotes++;
+    [self updateWindowState];
+}
+
+- (void)wiimoteDisconnected
+{
+    m_ConnectedWiimotes--;
+    [self updateWindowState];
+}
+
+@end
