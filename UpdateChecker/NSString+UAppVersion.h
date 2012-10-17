570b5278af01fc6ba7dc1b808d66fae8a62622d8
alexandr.serkov <alexandr.serkov@gmail.com>
2012-10-17 10:50:17 +0000

    Added UpdateChecker framework

---
+++ UpdateChecker/NSString+UAppVersion.h
@@ -0,0 +1,21 @@
+//
+//  NSString+UAppVersion.h
+//  UpdateChecker
+//
+//  Created by alxn1 on 17.10.12.
+//  Copyright 2012 alxn1. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import "UAppVersion.h"
+
+@interface NSString (UAppVersion)
+
++ (NSString*)stringWithAppVersion:(const UAppVersion*)version;
+
+- (id)initWithAppVersion:(const UAppVersion*)version;
+
+- (BOOL)parseAppVersion:(UAppVersion*)version;
+
+@end
---
+++ UpdateChecker/UAppVersion.h
@@ -0,0 +1,14 @@
+//
+//  UAppVersion.h
+//  UpdateChecker
+//
+//  Created by alxn1 on 17.10.12.
+//  Copyright 2012 alxn1. All rights reserved.
+//
+
+typedef struct UAppVersion {
+    int major;
+    int minor;
+    int patch;
+    int subPatch;
+} UAppVersion;
