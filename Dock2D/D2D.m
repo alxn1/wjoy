bc8a57d0581ce1022ab9f02709c7a5f9c0d4b842
alexandr.serkov <alexandr.serkov@gmail.com>
2013-11-01 12:13:49 +0000

    Added Dock2D project

---
+++ Dock2D/D2D.m
@@ -0,0 +1,127 @@
+/*
+ *  D2D.c
+ *  Dock2D
+ *
+ *  Created by alxn1 on 31.10.13.
+ *  Copyright 2013 alxn1. All rights reserved.
+ *
+ */
+
+#import "NSObject+ObjCRuntime.h"
+
+@interface _DOCKGlassFloorLayer : NSObject
+
+@end
+
+@implementation _DOCKGlassFloorLayer
+
++ (void)refreshDock
+{
+    NSString *script    = [[NSBundle bundleForClass:[self class]] pathForResource:@"refreshDock" ofType:@"scpt"];
+    NSTask   *task      = [[[NSTask alloc] init] autorelease];
+
+    [task setLaunchPath:@"/usr/bin/osascript"];
+    [task setArguments:[NSArray arrayWithObject:script]];
+    [task launch];
+}
+
++ (void)load
+{
+    NSAutoreleasePool *pool             = [[NSAutoreleasePool alloc] init];
+    Class              dockGlassClass   = NSClassFromString(@"DOCKGlassFloorLayer");
+
+    [dockGlassClass
+            addMethod:[self getMethod:@selector(updateFrame: iconOffset:)]
+                 name:@selector(interceptedUpdateFrame: iconOffset:)];
+
+    [dockGlassClass
+        swizzleMethod:@selector(updateFrame: iconOffset:)
+           withMethod:@selector(interceptedUpdateFrame: iconOffset:)];
+
+    [self refreshDock];
+    [pool release];
+}
+
++ (CALayer*)add2DBackgroundLayer:(CALayer*)parent
+{
+    CALayer   *result   = [CALayer layer];
+    CGColorRef black    = CGColorCreateGenericRGB(0.0f, 0.0f, 0.0f, 0.65f);
+    CGColorRef white    = CGColorCreateGenericRGB(1.0f, 1.0f, 1.0f, 0.75f);
+
+    [result setBorderWidth:1.5f];
+    [result setCornerRadius:5.0f];
+    [result setBackgroundColor:black];
+    [result setBorderColor:white];
+
+    CGColorRelease(black);
+    CGColorRelease(white);
+
+    [parent addSublayer:result];
+
+    return result;
+}
+
++ (void)fix2DSeparatorLayerImage:(CALayer*)separatorLayer
+{
+    if(![[[separatorLayer contents] className] isEqualToString:@"NSImage"])
+    {
+        NSImage *content = [[NSBundle bundleForClass:[self class]] imageForResource:@"separator"];
+
+        [separatorLayer setContents:content];
+    }
+}
+
++ (void)fix2DIndicatorLayer:(CALayer*)layer
+{
+    if(![[[layer contents] className] isEqualToString:@"NSImage"])
+    {
+        NSImage *content = [[NSBundle bundleForClass:[self class]] imageForResource:@"indicator"];
+
+        [layer setContents:content];
+    }
+}
+
++ (void)fix2DReflectionLayer:(CALayer*)layer
+{
+    CGRect reflectionsFrame = [layer frame];
+
+    if(reflectionsFrame.origin.y > 0.0f)
+    {
+        reflectionsFrame.origin.y = 0.0f;
+
+        [layer setFrame:reflectionsFrame];
+    }
+}
+
++ (void)fix2DIndicatorAndReflectionLayers:(NSArray*)layers
+{
+    for(CALayer *layer in layers)
+    {
+        if([[layer className] isEqualToString:@"DOCKIndicatorLayer"])
+            [self fix2DIndicatorLayer:layer];
+
+        if([[layer className] isEqualToString:@"CALayer"])
+            [self fix2DReflectionLayer:layer];
+    }
+}
+
+- (void)updateFrame:(CGRect)frame iconOffset:(unsigned int)offset
+{
+    CALayer *layer          = (CALayer*)self;
+    NSArray *sublayers      = [layer sublayers];
+    CALayer *separatorLayer = [self valueForKey:@"_separatorLayer"];
+    CALayer *frameLayer     = ([sublayers count] < 2)?
+                                    ([_DOCKGlassFloorLayer add2DBackgroundLayer:layer]):
+                                    ([sublayers objectAtIndex:1]);
+
+    frame.size.height = ((int)frame.size.height * 1.65f);
+
+    [layer setFrame:frame];
+    [separatorLayer setFrame:frame];
+    [frameLayer setFrame:CGRectMake(0.0, -5.0f, frame.size.width, frame.size.height + 5.0f)];
+
+    [_DOCKGlassFloorLayer fix2DSeparatorLayerImage:separatorLayer];
+    [_DOCKGlassFloorLayer fix2DIndicatorAndReflectionLayers:[[layer superlayer] sublayers]];
+}
+
+@end
---
+++ Dock2D/main.m
@@ -0,0 +1,77 @@
+#import <Cocoa/Cocoa.h>
+
+#import "mach_inject/mach_inject_bundle/mach_inject_bundle.h"
+#import "LoginItemsList.h"
+
+@interface Dock2D : NSObject
+
+- (void)run;
+
+@end
+
+@implementation Dock2D
+
+- (void)makeDock2D:(NSRunningApplication*)application
+{
+    NSString *bundlePath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"D2D.bundle"];
+
+    mach_inject_bundle_pid(
+        [bundlePath fileSystemRepresentation],
+        [application processIdentifier]);
+}
+
+- (void)makeLaunchedDock2D
+{
+    NSArray *apps = [NSRunningApplication runningApplicationsWithBundleIdentifier:@"com.apple.dock"];
+
+    for(NSRunningApplication *app in apps)
+        [self makeDock2D:app];
+}
+
+- (id)init
+{
+    self = [super init];
+
+    if(self == nil)
+        return nil;
+
+    [self makeLaunchedDock2D];
+
+    [[NSDistributedNotificationCenter defaultCenter]
+                                                addObserver:self
+                                                   selector:@selector(dockLaunchedNotification:)
+                                                       name:@"com.apple.desktop.ready"
+                                                     object:nil];
+
+    return self;
+}
+
+- (void)dealloc
+{
+    [[NSDistributedNotificationCenter defaultCenter] removeObserver:self];
+    [super dealloc];
+}
+
+- (void)dockLaunchedNotification:(NSNotification*)notification
+{
+    [self makeLaunchedDock2D];
+}
+
+- (void)run
+{
+    [[LoginItemsList userItemsList] addItemWithPath:[[[NSBundle mainBundle] bundleURL] path]];
+    [[NSRunLoop currentRunLoop] run];
+}
+
+@end
+
+int main(int argc, const char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    Dock2D            *app  = [[[Dock2D alloc] init] autorelease];
+
+    [app run];
+
+    [pool release];
+    return 0;
+}
