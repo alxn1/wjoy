987f9cf0c1d70932ed1067f3abc34cdb3489a877
alexandr.serkov <alexandr.serkov@f8454b35-cfc1-02ba-faa6-ef0c7441a174>
2014-02-26 19:00:30 +0000

    And now UserActivityNotifier work.

--- trunk/WJoy/UserActivityNotifier.m
+++ trunk/WJoy/UserActivityNotifier.m
@@ -8,8 +8,6 @@
 
 #import "UserActivityNotifier.h"
 
-#import <dlfcn.h>
-
 @implementation UserActivityNotifier
 
 + (UserActivityNotifier*)sharedNotifier
@@ -38,33 +36,13 @@
     [super dealloc];
 }
 
-- (void)updateSystemActivity
-{
-    typedef IOReturn (*DeclareUserActivityFn)(CFStringRef, IOPMUserActiveType, IOPMAssertionID*);
-
-    static BOOL                     isInit              = NO;
-    static DeclareUserActivityFn    declareUserActivity = NULL;
-    static CFStringRef              activityName        = (CFStringRef)@"WJoy Controller";
-
-    if(!isInit)
-    {
-		declareUserActivity	= dlsym(RTLD_DEFAULT, "IOPMAssertionDeclareUserActivity");
-        isInit              = YES;
-    }
-
-    if(declareUserActivity != NULL)
-        declareUserActivity(activityName, kIOPMUserActiveLocal, &m_AssertionID);
-    else
-        UpdateSystemActivity(UsrActivity);
-}
-
 - (void)notify
 {
     NSDate *now = [NSDate date];
 
-    if([now timeIntervalSinceDate:m_LastNotifyTime] >= 30.0)
+    if([now timeIntervalSinceDate:m_LastNotifyTime] >= 5.0)
     {
-        [self updateSystemActivity];
+        UpdateSystemActivity(UsrActivity);
 
         [m_LastNotifyTime release];
         m_LastNotifyTime = [now retain];
