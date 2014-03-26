47e8e2327cae46140dd39f01a6a47b38c96c2dcf
alexandr.serkov <alexandr.serkov@f8454b35-cfc1-02ba-faa6-ef0c7441a174>
2014-03-26 18:05:01 +0000

    new test utility for WiimoteC API.

---
+++ trunk/Wiimote/wc_main.c
@@ -0,0 +1,41 @@
+//
+//  main.c
+//  wc_test
+//
+//  Created by alxn1 on 26.03.14.
+//
+//
+
+#include "WiimoteC.h"
+
+#include <stdio.h>
+
+int main(int argC, char *argV[])
+{
+    wmc_initialize();
+    wmc_set_log_level(wmc_log_level_debug);
+    wmc_set_one_button_click_connection_enabled(1);
+
+    while(wmc_get_count_connected() == 0) {}
+
+    wmc_w_play_connect_effect(0);
+    wmc_w_set_led_mask(0, wmc_led_one);
+    while(wmc_get_count_connected() != 0)
+    {
+        if(wmc_w_is_button_pressed(0, wmc_button_left))     printf("L");
+        if(wmc_w_is_button_pressed(0, wmc_button_right))    printf("R");
+        if(wmc_w_is_button_pressed(0, wmc_button_up))       printf("U");
+        if(wmc_w_is_button_pressed(0, wmc_button_down))     printf("D");
+        if(wmc_w_is_button_pressed(0, wmc_button_a))        printf("a");
+        if(wmc_w_is_button_pressed(0, wmc_button_b))        printf("b");
+        if(wmc_w_is_button_pressed(0, wmc_button_plus))     printf("+");
+        if(wmc_w_is_button_pressed(0, wmc_button_minus))    printf("-");
+        if(wmc_w_is_button_pressed(0, wmc_button_home))     printf("H");
+        if(wmc_w_is_button_pressed(0, wmc_button_one))      printf("1");
+        if(wmc_w_is_button_pressed(0, wmc_button_two))      printf("2");
+    }
+
+    wmc_shutdown();
+    printf("\nOK\n");
+    return 0;
+}
